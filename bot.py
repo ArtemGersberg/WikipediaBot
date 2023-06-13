85c13ed31bbe3c9f1f705aaeda7651c5f2f33b7f
Artem Gershberg <135949583+ArtemGersberg@users.noreply.github.com>
2023-06-14 01:23:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,16 @@
+import aiogram.types
 import wikipedia
 from aiogram import Bot, Dispatcher, executor, types
 import re
-
-from aiogram.utils.callback_data import CallbackData
+import langdetect
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+#from aiogram.dispatcher.filters import Command
+#from aiogram.utils.callback_data import CallbackData
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from keyboards import keyboard , ininlinekeyboard, keyboard2,lang_markup
 from env import TOKEN
-from keyboards import ininlinekeyboard, keyboard, keyboard2
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
@@ -13,13 +18,16 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.reply("Отправьте мне любое слово, и я найду его значение на Wikipedia")
+    await message.reply("Отправьте мне любое слово, и я найду его значение на Wikipedia",reply_markup=aiogram.types.ReplyKeyboardRemove())
+
+@dp.message_handler(commands=['lang'])
+async def send_welcome(message: types.Message):
+    await message.reply("Выбериете язык ввода",reply_markup=lang_markup)
 
 
 @dp.message_handler(commands=['help'])
 async def send_welcome(message: types.Message):
-    await message.reply("Отправьте мне любое слово, и я найду его значение на Wikipedia")
-
+    await message.reply(f"Welcome to Wikipedia for the Telegram.\nThis bot can quickly search for the meanings of the words you are interested in.\nYou can also choose the input language or use the language auto-detection function (it is automotic).\nEnjoy using it)")
 
 @dp.message_handler()
 async def any_text_message(message: types.Message):
@@ -30,11 +38,6 @@ async def any_text_message(message: types.Message):
         await message.answer(txt, reply_markup=kb)
     else:
         await message.answer(txt)
-
-
-
-
-
 
 def getwiki(text):
     try:
@@ -56,7 +59,6 @@ def getwiki(text):
         wikitext2 = re.sub('\([^()]*\)', '', wikitext2)
         wikitext2 = re.sub('\{[^\{\}]*\}', '', wikitext2)
         return wikitext2, ny.url
-
 
     except Exception as e:
         return 'В энциклопедии нет информации об этом', None
