c736b4fa4ed8e2da6be2cded8449fc8960b2707a
Artem <gersbergartem@gmail.com>
2023-06-14 09:02:26 +0300

    wikibot

--- keyboards.py
+++ keyboards.py
@@ -16,15 +16,5 @@ i3=InlineKeyboardButton("Русский", callback_data="ru")
 lang_markup = InlineKeyboardMarkup(row_width=2)
 lang_markup.add(i3).add(i4)
 
-
-
-
-b1 = KeyboardButton("Английский" )
-b2 = KeyboardButton("Французский")
-keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-keyboard.add(b1).add(b2)
-
-keyboard2 = ReplyKeyboardMarkup(one_time_keyboard=True)
-keyboard2.insert(b1).insert(b2)
 ininlinekeyboard=InlineKeyboardMarkup().insert(i1).insert(i2)
 
