b348326308a248767d0605b21a0bf501976f024b
Artem <gersbergartem@gmail.com>
2023-06-14 22:09:02 +0300

    wikibot

--- bot.py
+++ bot.py
@@ -6,45 +6,79 @@ import langdetect
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-#from aiogram.dispatcher.filters import Command
-#from aiogram.utils.callback_data import CallbackData
+
+# from aiogram.dispatcher.filters import Command
+from aiogram.dispatcher import FSMContext
+# from aiogram.utils.callback_data import CallbackData
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from keyboards import keyboard , ininlinekeyboard, keyboard2,lang_markup
-from env import TOKEN
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+from keyboards import ininlinekeyboard, lang_markup
+
+TOKEN = "5977899411:AAGcb-0i30DnLoYOm03uRljSMDD_xBYpOfQ"
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=MemoryStorage())
 
 
-@dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    await message.reply("Отправьте мне любое слово, и я найду его значение на Wikipedia",reply_markup=aiogram.types.ReplyKeyboardRemove())
+@dp.message_handler(commands=['start'], state="*")
+async def send_welcome(message: types.Message, state: FSMContext):
+    await state.update_data(language="ru")
+    await message.reply("Отправьте мне любое слово, и я найду его значение на Wikipedia",
+                        reply_markup=aiogram.types.ReplyKeyboardRemove())
+    await state.set_state("ask_request")
 
-@dp.message_handler(commands=['lang'])
-async def send_welcome(message: types.Message):
-    await message.reply("Выбериете язык ввода",reply_markup=lang_markup)
+
+@dp.message_handler(commands=['lang'], state="*")
+async def setup_language(message: types.Message, state: FSMContext):
+    await message.reply("Выберите язык ввода", reply_markup=lang_markup)
+    await state.set_state("wait_language")
 
 
-@dp.message_handler(commands=['help'])
-async def send_welcome(message: types.Message):
-    await message.reply(f"Welcome to Wikipedia for the Telegram.\nThis bot can quickly search for the meanings of the words you are interested in.\nYou can also choose the input language or use the language auto-detection function (it is automotic).\nEnjoy using it)")
+@dp.message_handler(commands=['help'], state="*")
+async def get_help(message: types.Message):
+    await message.reply(
+        f"Welcome to Wikipedia for the Telegram.\nThis bot can quickly search for the meanings of the words you are interested in.\nYou can also choose the input language or use the language auto-detection function (it is automotic).\nEnjoy using it\n/start)")
 
-@dp.message_handler()
-async def any_text_message(message: types.Message):
-    txt, url = getwiki(message.text)
+
+@dp.message_handler(state="wait_language")
+async def set_language(message: types.Message, state: FSMContext):
+    lang_markup = message.text
+
+    if lang_markup == "English":
+        await message.reply("Chosen language: English")
+        await state.update_data(language="en")
+    elif lang_markup == "Русский":
+        await message.reply("Выбранный язык: Русский")
+        await state.update_data(language="ru")
+    elif lang_markup=="Français":
+        await message.reply("Langue choisie: Français")
+        await state.update_data(language="fr")
+    elif lang_markup=="Deutsch":
+        await message.reply("Ausgewählte Sprache: Deutsch")
+        await state.update_data(language="de")
+
+    await state.set_state("ask_request")
+
+
+@dp.message_handler(state="ask_request")
+async def find_on_wiki(message: types.Message, state: FSMContext):
+    data = await state.get_data()
+    language = data['language']
+
+    txt, url = getwiki(message.text, language)
     if url:
-        button = InlineKeyboardButton('Ссылка на wiki', url=url)
+        button = InlineKeyboardButton('Ссылка на статью Wikipedia', url=url)
         kb = InlineKeyboardMarkup().add(button)
         await message.answer(txt, reply_markup=kb)
     else:
         await message.answer(txt)
 
-def getwiki(text):
+
+def getwiki(text, language: str):
     try:
-        language = "ru"
         wikipedia.set_lang(language)
         ny = wikipedia.page(text)
-
         wikitext = ny.content[:1000]
         wikimas = wikitext.split('.')
         wikimas = wikimas[:-1]
@@ -59,6 +93,7 @@ def getwiki(text):
         wikitext2 = re.sub('\([^()]*\)', '', wikitext2)
         wikitext2 = re.sub('\{[^\{\}]*\}', '', wikitext2)
         return wikitext2, ny.url
+
 
     except Exception as e:
         return 'В энциклопедии нет информации об этом', None
--- keyboards.py
+++ keyboards.py
@@ -5,16 +5,24 @@ from aiogram.types import \
     InlineKeyboardMarkup, \
     InlineKeyboardButton
 
+i1 = InlineKeyboardButton(f'Читать дальше... ', callback_data='i2')
+i2 = InlineKeyboardButton(f'Перейти на сайт со статьёй→', callback_data='i2')
 
+i4 = InlineKeyboardButton("English", callback_data="en")
+i3 = InlineKeyboardButton("Русский", callback_data="ru")
 
-i1 = InlineKeyboardButton(f'Читать дальше... ',callback_data='i2')
-i2 = InlineKeyboardButton(f'Перейти на сайт со статьёй→',callback_data='i2')
+k1 = KeyboardButton("English")
+k2 = KeyboardButton("Русский")
+k3 = KeyboardButton("Français")
+k4 = KeyboardButton("Deutsch")
+k5 = KeyboardButton("Español")
+k6 = KeyboardButton("中文")
+k7 = KeyboardButton("العربية")
+k8 = KeyboardButton("Italiano")
+k9 = KeyboardButton("िन्दी")
+# lang_markup = InlineKeyboardMarkup(row_width=2)
+# lang_markup.add(i3).add(i4)
 
-i4=InlineKeyboardButton("English", callback_data="en")
-i3=InlineKeyboardButton("Русский", callback_data="ru")
+lang_markup = ReplyKeyboardMarkup().add(k1).add(k2).add(k3).add(k4).add(k5).add(k6).add(k7).add(8).add(k9)
 
-lang_markup = InlineKeyboardMarkup(row_width=2)
-lang_markup.add(i3).add(i4)
-
-ininlinekeyboard=InlineKeyboardMarkup().insert(i1).insert(i2)
-
+ininlinekeyboard = InlineKeyboardMarkup().insert(i1).insert(i2)
