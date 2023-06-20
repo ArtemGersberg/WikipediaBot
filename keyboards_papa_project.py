2684ebf9193257d786da81a250876579a1436099
Artem <gersbergartem@gmail.com>
2023-06-20 21:42:48 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	bot.py
    #	bot_test.py
    #	keyboards.py

--- keyboards_papa_project.py
+++ keyboards_papa_project.py
@@ -4,6 +4,30 @@ from aiogram.types import \
 
 
 k1 = KeyboardButton("Расчет количества температурно-усадочных швов")
+k2 = KeyboardButton("Расчёт количества арматуры(на квадратный метр)")
 
 
-lang_markup12 = ReplyKeyboardMarkup().add(k1)
+lang_markup1 = ReplyKeyboardMarkup().add(k1).add(k2)
+
+
+
+a1 = KeyboardButton("Однослойное армирование")
+a2 = KeyboardButton("Двхслойное армирование")
+lang_markup2 = ReplyKeyboardMarkup().add(a1).add(a2)
+
+
+
+dem1 = KeyboardButton("8")
+dem2 = KeyboardButton("10")
+dem3 = KeyboardButton("12")
+dem4 = KeyboardButton("14")
+dem5 = KeyboardButton("16")
+lang_markup3 = ReplyKeyboardMarkup().add(dem1).add(dem2).add(dem3).add(dem4).add(dem5)
+
+
+shag1 = KeyboardButton("100мм")
+shag2 = KeyboardButton("150мм")
+shag3 = KeyboardButton("200мм")
+shag4 = KeyboardButton("250мм")
+shag5 = KeyboardButton("300мм")
+lang_markup4 = ReplyKeyboardMarkup().add(shag1).add(shag2).add(shag3).add(shag4).add(shag5)
--- project papa.py
+++ project papa.py
@@ -6,7 +6,7 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardRemove
 from aiogram.utils import executor
-from keyboards_papa_project import lang_markup12
+from keyboards_papa_project import lang_markup1,lang_markup2,lang_markup3,lang_markup4
 
 TOKEN="5701505172:AAEFVT76AqvIMBCUZgJ9cY39BVumvFJNCIM"
 bot = Bot(token=TOKEN)
@@ -15,7 +15,7 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 
 @dp.message_handler(commands=['start'], state="*")
 async def setup_language(message: types.Message, state: FSMContext):
-    await message.answer("Выбор действия", reply_markup=lang_markup12)
+    await message.answer("Выбор действия", reply_markup=lang_markup1)
     await state.set_state("wait_knopka")
 
 @dp.message_handler(state='площадь')
@@ -45,11 +45,104 @@ async def age_process(message: types.Message, state: FSMContext):
 
 @dp.message_handler(state="wait_knopka")
 async def set_language(message: types.Message, state: FSMContext):
-    lang_markup12 = message.text
-    if lang_markup12 == "Расчет количества температурно-усадочных швов":
+    lang_markup1 = message.text
+    if lang_markup1 == "Расчет количества температурно-усадочных швов":
         await message.answer("Эта функция считает количество температурно-усадочных швов\nДля того чтобы получить их количество,введите все данные, которые запрашивает бот и он выведет ответ",reply_markup=ReplyKeyboardRemove())
         await message.answer("Введите площадь(в м²)")
         await state.set_state("площадь")
+    elif lang_markup1 == "Расчёт количества арматуры(на квадратный метр)":
+        await message.answer("Выбирите скольки слойное армирование хотите сделать",reply_markup=lang_markup2)
+        await state.set_state("Скольки слойное армироване")
+
+@dp.message_handler(state="Скольки слойное армироване")
+async def set_language(message: types.Message, state: FSMContext):
+    lang_markup2 = message.text
+    if lang_markup2 == "Однослойное армирование":
+        await message.answer(
+            "Эта функция считает количество арматуры на квадратный метр\nДля того чтобы получить это количество,введите все данные, которые запрашивает бот и он выведет ответ",
+            reply_markup=ReplyKeyboardRemove())
+        await message.answer("Выберите диаметр арматуры(в мм)", reply_markup=lang_markup3)
+        await state.set_state("однослойное армирование")
+
+
+@dp.message_handler(state='однослойное армирование')
+async def age_process(message: types.Message, state: FSMContext):
+    c = message.text
+    if isinstance(c, str) and c.isdigit():
+        await state.update_data({'однослойное армирование': float(c)})
+        if lang_markup3=="8":
+            dem20 = 0.395
+            await state.update_data({'масса арматуры': float(dem20)})
+            await message.answer("Теперь введите шаг арматуры",reply_markup=lang_markup4)
+            await state.set_state("шаг арматуры")
+        elif lang_markup3=="10":
+            dem20 = 0.617
+            await state.update_data({'масса арматуры': float(dem20)})
+            await message.answer("Теперь введите шаг арматуры", reply_markup=lang_markup4)
+            await state.set_state("шаг арматуры")
+        elif lang_markup3=="12":
+            dem20 = 0.888
+            await state.update_data({'масса арматуры': float(dem20)})
+            await message.answer("Теперь введите шаг арматуры", reply_markup=lang_markup4)
+            await state.set_state("шаг арматуры")
+        elif lang_markup3=="14":
+            dem20 = 1.210
+            await state.update_data({'масса арматуры': float(dem20)})
+            await message.answer("Теперь введите шаг арматуры", reply_markup=lang_markup4)
+            await state.set_state("шаг арматуры")
+        elif lang_markup3=="16":
+            dem20 = 1.580
+            await state.update_data({'масса арматуры': float(dem20)})
+            await message.answer("Теперь введите шаг арматуры", reply_markup=lang_markup4)
+            await state.set_state("шаг арматуры")
+    else:
+        await message.reply("Вы ввели не число. Повторите попытку")
+
+
+
+@dp.message_handler(state='шаг арматуры')
+async def age_process(message: types.Message, state: FSMContext):
+    d = message.text
+    if isinstance(d, str) and d.isdigit():
+        if lang_markup4 == "100мм":
+            shag20 = 100
+            await state.update_data({'шаг': float(shag20)})
+            await state.set_state("формула 2ой кнопки")
+        elif lang_markup4 == "150мм":
+            shag20 = 150
+            await state.update_data({'шаг': float(shag20)})
+            await state.set_state("формула 2ой кнопки")
+        elif lang_markup4 == "150мм":
+            shag20 = 200
+            await state.update_data({'шаг': float(shag20)})
+            await state.set_state("формула 2ой кнопки")
+        elif lang_markup4 == "150мм":
+            shag20 = 250
+            await state.update_data({'шаг': float(shag20)})
+            await state.set_state("формула 2ой кнопки")
+        elif lang_markup4 == "150мм":
+            shag20 = 300
+            await state.update_data({'шаг': float(shag20)})
+            await state.set_state("формула 2ой кнопки")
+    else:
+        await message.reply("Вы ввели не число. Повторите попытку")
+
+
+
+@dp.message_handler(state='формула 2ой кнопки')
+async def age_process(message: types.Message, state: FSMContext):
+    data = await state.get_data()
+    sh = float(data["шаг"])
+    data = await state.get_data()
+    de = float(data["масса арматуры"])
+    await message.answer("Количество арматуры - " + str((((300/float(sh))*2)*float(de))/9))
+
+
+
+
+
+
+
 
 
 
